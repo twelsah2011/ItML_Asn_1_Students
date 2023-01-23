f7e669d2238d7f21db02a4007641074ef67abfef
twelsah2011 <ehsiao3269@gmail.com>
2023-01-22 19:27:37 -0700

    update

--- ml_utils.py
+++ ml_utils.py
@@ -4,13 +4,70 @@ import math
 import sklearn.datasets
 import ipywidgets as widgets
 
-##Seaborn for fancy plots. 
-#%matplotlib inline
+## Seaborn for fancy plots. 
+# %matplotlib inline
 import matplotlib.pyplot as plt
 import seaborn as sns
 plt.rcParams["figure.figsize"] = (8,8)
 
 class edaDF:
+    """
+    A class used to perform common EDA tasks
+
+    ...
+
+    Attributes
+    ----------
+    data : dataframe
+        a dataframe on which the EDA will be performed
+    target : str
+        the name of the target column
+    cat : list
+        a list of the names of the categorical columns
+    num : list
+        a list of the names of the numerical columns
+
+    Methods
+    -------
+    setCat(catList)
+        sets the cat variable listing the categorical column names to the list provided in the argument catList
+        
+        Parameters
+        ----------
+        catlist : list
+            The list of column names that are categorical
+
+    setNum(numList)
+        sets the cat variable listing the categorical column names to the list provided in the argument catList
+        
+        Parameters
+        ----------
+        numlist : list
+            The list of column names that are numerical
+
+    countPlots(self, splitTarg=False, show=True)
+        generates countplots for the categorical variables in the dataset 
+
+        Parameters
+        ----------
+        splitTarg : bool
+            If true, use the hue function in the countplot to split the data by the target value
+        show : bool
+            If true, display the graphs when the function is called. Otherwise the figure is returned.
+    
+    histPlots(self, splitTarg=False, show=True)
+        generates countplots for the categorical variables in the dataset 
+
+        Parameters
+        ----------
+        splitTarg : bool
+            If true, use the hue function in the countplot to split the data by the target value
+        show : bool
+            If true, display the graphs when the function is called. Otherwise the figure is returned. 
+
+    fullEDA()
+        Displays the full EDA process. 
+    """
     def __init__(self, data, target):
         self.data = data
         self.target = target
@@ -68,23 +125,34 @@ class edaDF:
             figure.show()
         return figure
 
-    def heatmap(self, splitTarg = False, show = True) :
-        sns.heatmap(center = 0, linewidths = .5, annot = True, cmap = 'Y1GnBu', yticklabels = True)
-        color = 3
-        plt.show()
+
+    def summary(self,include='all',exclude=None, percentiles=[.25, .5, .75]):
+        self.des = pd.DataFrame(self.num)
+        return self.des.describe(include=include, exclude=exclude, percentiles=percentiles)
+
+
+    def correlation(self):
+        corr = self.data.corr()
+        sns.heatmap(corr, annot = True, cmap = 'coolwarm')
+
+
 
 
     def fullEDA(self):
-        out1 = widgets.Output()
-        out2 = widgets.Output()
-        out3 = widgets.Output()
-        out4 = widgets.Output()
+        out1 = widgets.Output() # info
+        out2 = widgets.Output() # categorical
+        out3 = widgets.Output() # numerical
+        out4 = widgets.Output() # describe
+        out5 = widgets.Output() # correlation
+        out6 = widgets.Output() # boxplot(cat)
 
-        tab = widgets.Tab(children = [out1, out2, out3, out4])
+        tab = widgets.Tab(children = [out1, out2, out3, out4, out5, out6])
         tab.set_title(0, 'Info')
         tab.set_title(1, 'Categorical')
         tab.set_title(2, 'Numerical')
-        tab.set_title(3, 'heatmap')
+        tab.set_title(3, 'Describe')
+        tab.set_title(4, 'Correlation')
+        tab.set_title(5, 'Boxplot')
         display(tab)
 
         with out1:
@@ -97,7 +165,14 @@ class edaDF:
         with out3:
             fig3 = self.histPlots(kde=True, show=False)
             plt.show(fig3)
-        
+
         with out4:
-            fig4 = self.heatmap(splitTarg=True, show=False)
-            plt.show(fig4)
+            self.summary()
+        
+        with out5:
+            plt.show(self.correlation())
+
+        
+        with out6:
+            plt.show(self.data.boxplot())
+
